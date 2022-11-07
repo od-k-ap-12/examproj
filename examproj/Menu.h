545010e6a3f9c4a9b4e7508cbba4fcc24d6125db
od-k-ap-12 <112928816+od-k-ap-12@users.noreply.github.com>
2022-11-07 12:05:49 +0200

    Update Menu.h

--- examproj/Menu.h
+++ examproj/Menu.h
@@ -6,15 +6,34 @@
 #include <vector>
 using namespace std;
 
+//Главное меню
 void TitleScreen(list<User*>& Users, vector<string>& Categories, vector<Test>& Tests);
+
+//Меню админа
 void AdminMenu(list<User*>& Users, Admin* Admin, vector<string>& Categories, vector<Test>& Tests);
+
+//Меню пользователя
 void UserMenu(list<User*>& Users, User* User, vector<string>& Categories, vector<Test>& Tests);
+
+//Регистрация
 void SignUp(list<User*>& Users, vector<string>& Categories, vector<Test>& Tests);
+
+//Вход в аккаунт админа
 void SignInAdmin(list<User*>& Users, Admin* Admin, vector<string>& Categories, vector<Test>& Tests);
+
+//Вход в аккаунт пользователя
 void SignIn(list<User*>& Users, vector<string>& Categories, vector<Test>& Tests);
+
+//Проверка пользователя
 bool UserCheck(string _Login, string _Password, list<User*>& Users);
+
+//Проверка на дубликат логина
 bool LoginCheck(string _Login, list<User*>& Users);
+
+//Искать логин
 bool SearchLogin(string _Login, list<User*>& Users);
+
+//Искать пользователя
 bool SearchUser(string _Login, string _Password, list<User*>& Users);
 
 
