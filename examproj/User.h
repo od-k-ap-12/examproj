7a1e92e71a5ec318abff0dc37affea126c77a157
od-k-ap-12 <112928816+od-k-ap-12@users.noreply.github.com>
2022-11-07 12:01:17 +0200

    Update User.h

--- examproj/User.h
+++ examproj/User.h
@@ -19,12 +19,28 @@ class User
 public:
 	User(string _Login, string _Password, string _FirstName, string _LastName, string _Address, string _PhoneNumber);
 	User(string _Login, string _Password);
+	
+	//Посмотреть все свои результаты
 	void PrintResults();
+	
+	//Пройти тест
 	void PassTest(list<User*>& Users, vector<string>& Categories, vector<Test>& Tests);
+	
+	//Достать логин
 	string GetLogin();
+	
+	//Достать пароль
 	string GetPassword();
+	
+	//Изменить пользователя
 	void SetUser(string _Login, string _Password, string _FirstName, string _LastName, string _Address, string _PhoneNumber);
+	
+	//Сохранить пользователя в файл
 	void SaveUserToFile();
+	
+	//Сохранить результат (вызывается после каждого проходжения теста)
 	void SaveResult(string TestTitle, int Result);
+	
+	
 };
 
